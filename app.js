477b1baae837bd334c921c615f97ce9764c08772
Jordan Burch <Jordanburch101@gmail.com>
2021-03-17 16:35:12 +1300

    tested scrollto

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 const swup = new Swup();
 
 
+///////////////////////////////
+/////////// GSAP ////////////
+//////////////////////////////
+myWork = document.getElementsByClassName('myWork')
 
-// GSAP
+for (var i = 0; i < myWork.length; i++) {
+    myWork[i].addEventListener('click', (e) => {
+        TweenLite.to(window, 2, {scrollTo:400});
+    });
+}
 
 // Hero anim
 gsap.from('#svg-phone', {opacity: 0, duration: 3, x: 1550, ease: "power3.out", delay: 1.5});
@@ -12,4 +20,3 @@ gsap.from('.hero-text', {opacity: 0, duration: 2, y: -100, delay: 1});
 gsap.from('.hero-tagline', {opacity: 0, duration: 2, y: -50, ease: "power3.out", delay: 2});
 gsap.from('.hero-cta', {opacity: 0, duration: 2, x: -500, ease: "power3.out", delay: 3.5});
 gsap.from('.hero-buttons', {opacity: 0, duration: 2, y: -50, ease: "power1.out", delay: 3.5});
-
