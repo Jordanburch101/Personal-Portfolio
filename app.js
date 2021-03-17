348af19ef85ebcb3a1b2e1965774225641e20f2a
Jordan Burch <Jordanburch101@gmail.com>
2021-03-17 14:34:01 +1300

    changed home animation delay

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const swup = new Swup();
 // GSAP
 
 // Hero anim
-gsap.from('#svg-phone', {opacity: 0, duration: 3, x: 1550, ease: "power3.out"});
-gsap.from('#svg-monitor', {opacity: 0, duration: 2, x: 550, ease: "power3.out"});
-gsap.from('.hero-text', {opacity: 0, duration: 2, y: -200, ease: "power1. out"});
-gsap.from('.hero-tagline', {opacity: 0, duration: 2, y: -50, ease: "power3.out"});
-gsap.from('.hero-cta', {opacity: 0, duration: 2, x: -500, ease: "power3.out", delay: 1});
-gsap.from('.hero-buttons', {opacity: 0, duration: 2, y: -50, ease: "power1.out", delay: 1});
+gsap.from('#svg-phone', {opacity: 0, duration: 3, x: 1550, ease: "power3.out", delay: 1.5});
+gsap.from('#svg-monitor', {opacity: 0, duration: 2, x: 550, ease: "power3.out", delay: 1});
+gsap.from('.hero-text', {opacity: 0, duration: 2, y: -100, delay: 1});
+gsap.from('.hero-tagline', {opacity: 0, duration: 2, y: -50, ease: "power3.out", delay: 2});
+gsap.from('.hero-cta', {opacity: 0, duration: 2, x: -500, ease: "power3.out", delay: 3.5});
+gsap.from('.hero-buttons', {opacity: 0, duration: 2, y: -50, ease: "power1.out", delay: 3.5});
 
