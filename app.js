1942c0175dd859f506443b400757f61b25933a25
Jordan Burch <Jordanburch101@gmail.com>
2021-03-18 22:11:01 +1300

    fixed reloading of js

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
-// SWUP init
-const swup = new Swup();
+
 
 
 ///////////////////////////////
 /////////// GSAP ////////////
 //////////////////////////////
-myWork = document.getElementsByClassName('myWork')
+function heroAnimation() {
+    console.log('DOM fully loaded and parsed');
+    gsap.from('#svg-phone', {opacity: 0, duration: 3, x: 1550, ease: "power3.out", delay: 3});
+    gsap.from('#svg-monitor', {opacity: 0, duration: 3, x: 550, ease: "power3.out", delay: .5});
+    gsap.from('.hero-text', {opacity: 0, duration: 2, y: -100, delay: .2});
+    gsap.from('.hero-tagline', {opacity: 0, duration: 2, y: -50, ease: "power3.out", delay: 1});
+    gsap.from('.hero-cta', {opacity: 0, duration: 2, x: -500, ease: "power3.out", delay: 2});
+    gsap.from('.hero-buttons', {opacity: 0, duration: 2, y: -50, ease: "power1.out", delay: 1.5});
 
-for (var i = 0; i < myWork.length; i++) {
-    myWork[i].addEventListener('click', (e) => {
-        TweenLite.to(window, 2, {scrollTo:400});
-    });
+}
+function contactAnimation() {
+    
 }
 
-// Hero anim
-gsap.from('#svg-phone', {opacity: 0, duration: 3, x: 1550, ease: "power3.out", delay: 1.5});
-gsap.from('#svg-monitor', {opacity: 0, duration: 2, x: 550, ease: "power3.out", delay: 1});
-gsap.from('.hero-text', {opacity: 0, duration: 2, y: -100, delay: 1});
-gsap.from('.hero-tagline', {opacity: 0, duration: 2, y: -50, ease: "power3.out", delay: 2});
-gsap.from('.hero-cta', {opacity: 0, duration: 2, x: -500, ease: "power3.out", delay: 3.5});
-gsap.from('.hero-buttons', {opacity: 0, duration: 2, y: -50, ease: "power1.out", delay: 3.5});
+
+///////////////////////////////
+/////////// SWUP ////////////
+//////////////////////////////
+
+document.addEventListener("DOMContentLoaded", () => {
+    function init() {
+        // Get Page Name
+        const path = window.location.pathname;
+        const page = path.split("/").pop();
+        console.log( page );
+
+        if(page === "index.html") {
+            heroAnimation();
+        }
+        if(page === "contact.html") {
+            contactAnimation();
+        }
+    }
+    
+
+    const swup = new Swup();
+        init();
+    swup.on('contentReplaced', init);
+});
+
